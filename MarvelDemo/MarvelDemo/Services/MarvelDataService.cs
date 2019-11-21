15ba371b7c2c2f150423753fc11c3f5a999f1d48
Aaron Saunders <ASaunders2@my.wctc.edu>
2019-11-20 19:45:16 -0600

    Add project files.

---
+++ MarvelDemo/MarvelDemo/Services/MarvelDataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MarvelDemo.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MarvelDemo.Services
+{
+    public class MarvelDataService : IMarvelDataService
+    {
+        // TODO: Add your Marvel Developer Account keys
+        const string _API_PRIVATE_KEY = "e4c7e6f3829adaa09677323a8716226bbf4c619e";
+        const string _API_PUBLIC_KEY = "10de83b086c87909f37ef4ca92d7c554";
+
+        readonly IHashService _hashService;
+
+        public MarvelDataService(IHashService hashService)
+        {
+            _hashService = hashService;
+        }
+
+        public async Task<IEnumerable<Comic>> GetComicsBySeries(int seriesId, string orderBy = null)
+        {
+            var ts = Guid.NewGuid().ToString();
+            var hash = _hashService.CreateMd5Hash(ts + _API_PRIVATE_KEY + _API_PUBLIC_KEY);
+
+            if (string.IsNullOrWhiteSpace(orderBy))
+                orderBy = "issueNumber";
+
+            var url =
+                $@"http://gateway.marvel.com/v1/public/series/{seriesId}/comics?orderBy={orderBy}&apikey={_API_PUBLIC_KEY}&hash={hash}&ts={ts}";
+            
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+            
+            var responseObject = JObject.Parse(response);
+
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<IEnumerable<Comic>>(responseObject["data"]["results"].ToString()));
+        }
+    }
+}
---
+++ MarvelDemo/MarvelDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MarvelDemo.Models;
+
+namespace MarvelDemo.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        ObservableCollection<Character> _characters;
+        public ObservableCollection<Character> Characters
+        {
+            get { return _characters; }
+            set
+            {
+                _characters = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+
+        public void Init()
+        {
+            LoadCharacters();
+        }
+
+        void LoadCharacters()
+        {
+            Characters = new ObservableCollection<Character>();
+            
+            // Hard-coded list of Characters instead of pulling from the API so we can get just the specific Avengers we want to display:
+            Characters.Add(new Character
+            {
+                Id = 1009368,
+                Name = "Ezio Is Our Ally",
+                IconPath = "ironman_52.png",
+                SeriesId = 2029,
+                Thumbnail =
+                    new Image {Path = "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/standard_medium", Extension = "jpg"}
+            });
+            Characters.Add(new Character
+            {
+                Id = 1009220,
+                Name = "Leonardo is our most valuble asset",
+                IconPath = "captainamerica_52.png",
+                SeriesId = 1996,
+                Thumbnail =
+                    new Image {Path = "http://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087/standard_medium", Extension = "jpg"}
+            });
+            Characters.Add(new Character
+            {
+                Id = 1009664,
+                Name = "The Brotherhood is our family",
+                IconPath = "thor_52.png",
+                SeriesId = 2083,
+                Thumbnail =
+                    new Image {Path = "http://i.annihil.us/u/prod/marvel/i/mg/d/d0/5269657a74350/standard_medium", Extension = "jpg"}
+            });
+            Characters.Add(new Character
+            {
+                Id = 1009652,
+                Name = "Viviere is our ally",
+                IconPath = "thor_52.png", // thanos
+                SeriesId = 2116,
+                Thumbnail =
+                    new Image { Path = "http://i.annihil.us/u/prod/marvel/i/mg/6/40/5274137e3e2cd", Extension = "jpg" }
+            });
+            Characters.Add(new Character
+            {
+                Id = 1009208,
+                Name = "The Brotherhood is our family",
+                IconPath = "thor_52.png", //Brood
+                SeriesId = 22727,
+                Thumbnail =
+                    new Image { Path = "http://i.annihil.us/u/prod/marvel/i/mg/d/40/5260321259f91", Extension = "jpg" }
+            });
+        }
+    }
+}
